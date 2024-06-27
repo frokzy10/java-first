57cb4b97b0ccbbcff92dcf235ad10f2a75b51db1
Нурдин Бакытбеков <nurdinbakytbekov@MacBook-Air-Nurdin.local>
2024-06-27 19:14:17 +0600

    Изучил как работает Type Casting

---
+++ src/baseOfJava/TypeCasting/TypeCasting.java
@@ -0,0 +1,31 @@
+package baseOfJava.TypeCasting;
+
+class Animal {
+    public void bark(){
+        System.out.println("Bark");
+    }
+}
+class Dog extends Animal{
+    public void eat(){
+        System.out.println("Eat");
+    }
+}
+
+public class TypeCasting {
+    public static void main(String[] args) {
+//          Upcasting
+//        Dog dog = new Dog();
+//        Animal animal = dog;
+//        dog.bark();
+//        dog.eat();
+
+//          DownCasting
+//        Dog dog2 = (Dog) animal;
+//        dog2.bark();
+
+//          ErrorCasting
+        Animal a = new Animal();
+        Dog d = (Dog) a;
+        d.eat();
+    }
+}
